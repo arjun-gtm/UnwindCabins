5de4f16a80edbc79316993607375c9aa0d08681f
arjun-gtm <gtmarjun10@gmail.com>
2026-05-11 20:48:18 +0545

    Update

--- server/middleware/adminAuth.js
+++ server/middleware/adminAuth.js
@@ -1,6 +1,6 @@
 import { auth } from './authMiddleware.js';
 
 export const adminAuth = (req, res, next) => {
-  if (req.user.role !== 'admin') return res.status(403).json({ message: 'Admin access required' });
+  if (req.user.role !== 'admin') return res.status(403).json({ message: 'Admin access required!' });
   next();
 };
